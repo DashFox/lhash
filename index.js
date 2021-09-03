8279ff0ae3b393c7a4f8483d9dd21ca9420ba62a
Jay <f1skdev@gmail.com>
2021-09-03 23:31:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Helpers and connstants
 const sha256 = require("sha256")
 var dlog = function(){}
 
+// Exports
 exports.lhash128 = function(string) {
     return encode(string, 128)
 }
@@ -17,6 +19,7 @@ exports.lhashCustom = function(string, times, debug = false) {
     return encode(string, times, debug)
 }
 
+// Define the encode function
 const encode = function(string, times, debug = false) {
     // Declare short vars and make sure they are the correct type
     if(debug) dlog = console.log;
@@ -24,15 +27,17 @@ const encode = function(string, times, debug = false) {
     let t = Number(times);
     dlog("[lhash] Input string: " + s)
     dlog("[lhash] Layers: " + t)
+    // Run (t) times \\ layers
     for (let step = 0; step < t; step++) {
         s = magic(s)
         dlog("[lhash] Hashing... Layer: " + step + "\\" + t +": " + s )
       }
+      // Return the layered hash
       dlog("[lhash] Fínal hash: " + s)
       return s
       
 }
-
+// Converts a string to b64
 function magic(str) {
     let b64 = Buffer.from(`${str}`).toString('base64')
     dlog("[lhash] Base64: " + b64)
